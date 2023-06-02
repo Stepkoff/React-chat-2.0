1ebcee638d6bcf169ec7df0b8752ae50b80a78b1
Stepan Rudiak <stepan.rudiak95@gmail.com>
2023-06-02 23:01:44 +0200

    route bug fix vercel

--- src/app/routes/PrivateRoute.tsx
+++ src/app/routes/PrivateRoute.tsx
@@ -1,14 +1,18 @@
-import {Navigate, useLocation} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import {useAppSelector} from "@/shared/model";
 
 type PrivateRouteProps = {
   children: JSX.Element,
 }
+// export const PrivateRoute = ({children}:PrivateRouteProps) => {
+//   const authData = useAppSelector(state => state.authSlice.authData);
+//   const location = useLocation();
+//   const url = new URLSearchParams();
+//   url.set('redirect', location.pathname + location.search);
+//   return authData ? children : <Navigate to={{pathname: '/signIn', search: url.toString()}}/>
+// };
+
 export const PrivateRoute = ({children}:PrivateRouteProps) => {
   const authData = useAppSelector(state => state.authSlice.authData);
-  const location = useLocation();
-  const url = new URLSearchParams();
-  url.set('redirect', location.pathname + location.search);
-  return authData ? children : <Navigate to={{pathname: '/signIn', search: url.toString()}}/>
+  return authData ? children : <Navigate to={'/signIn'}/>
 };
-
