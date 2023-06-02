eef5bb516c9ee7a281ec653a49aead77eca36010
Stepan Rudiak <stepan.rudiak95@gmail.com>
2023-06-02 23:53:40 +0200

    route bug fix vercel 5

--- src/app/routes/PublicRoute.tsx
+++ src/app/routes/PublicRoute.tsx
@@ -1,17 +1,17 @@
-import {Navigate} from "react-router-dom";
+import {Navigate, useLocation} from "react-router-dom";
 import {useAppSelector} from "@/shared/model";
 
 type PublicRouteProps = {
   children: JSX.Element
 }
-// export const PublicRoute = ({children}:PublicRouteProps) => {
-//   const authData = useAppSelector(state => state.authSlice.authData);
-//   const location = useLocation();
-//   const url = new URLSearchParams(location.search.slice(1));
-//   return authData ? <Navigate to={url.get('redirect') || '/'}/> : children
-// };
-
 export const PublicRoute = ({children}:PublicRouteProps) => {
   const authData = useAppSelector(state => state.authSlice.authData);
-  return authData ? <Navigate to={'/'}/> : children
+  const location = useLocation();
+  const url = new URLSearchParams(location.search.slice(1));
+  return authData ? <Navigate to={url.get('redirect') || '/'}/> : children
 };
+
+// export const PublicRoute = ({children}:PublicRouteProps) => {
+//   const authData = useAppSelector(state => state.authSlice.authData);
+//   return authData ? <Navigate to={'/'}/> : children
+// };
